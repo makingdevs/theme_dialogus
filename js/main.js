5a440508be1691d12972f3c550e45fec323ebc2f
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-06-03 12:03:21 -0500

    seleccionando tema

---
+++ js/main.js
@@ -0,0 +1,424 @@
+/*-----------------------------------------------------------------------------------
+
+    Theme Name: Coach - Life Coach and Courses Template
+    Description: Life Coach and Courses Template
+    Author: Chitrakoot Web
+    Version: 1.0
+
+    /* ----------------------------------
+
+    JS Active Code Index
+            
+        01. Preloader
+        02. Sticky Header
+        03. Scroll To Top
+        04. Parallax
+        05. Video
+        06. Resize function
+        07. FullScreenHeight function
+        08. ScreenFixedHeight function
+        09. FullScreenHeight and screenHeight with resize function
+        10. Sliders
+        11. Tabs
+        12. CountUp
+        13. Countdown
+        14. Isotop
+        
+    ---------------------------------- */    
+
+(function($) {
+
+    "use strict";
+
+    var $window = $(window);
+
+        /*------------------------------------
+            01. Preloader
+        --------------------------------------*/
+
+        $('#preloader').fadeOut('normall', function() {
+            $(this).remove();
+        });
+
+        /*------------------------------------
+            02. Sticky Header
+        --------------------------------------*/
+
+        $window.on('scroll', function() {
+            var scroll = $window.scrollTop();
+            var logochange = $(".navbar-brand img");
+            if (scroll <= 50) {
+                $("header").removeClass("scrollHeader").addClass("fixedHeader");
+                logochange.attr('src', 'img/logos/logo-inner.png');
+            } 
+            else {
+                $("header").removeClass("fixedHeader").addClass("scrollHeader");
+                logochange.attr('src', 'img/logos/logo.png');
+            }
+        });
+
+
+        /*------------------------------------
+            03. Scroll To Top
+        --------------------------------------*/
+
+        $window.on('scroll', function() {
+            if ($(this).scrollTop() > 500) {
+                $(".scroll-to-top").fadeIn(400);
+
+            } else {
+                $(".scroll-to-top").fadeOut(400);
+            }
+        });
+
+        $(".scroll-to-top").on('click', function(event) {
+            event.preventDefault();
+            $("html, body").animate({
+                scrollTop: 0
+            }, 600);
+        });
+
+
+        /*------------------------------------
+            04. Parallax
+        --------------------------------------*/
+
+        // sections background image from data background
+        var pageSection = $(".parallax,.bg-img");
+        pageSection.each(function(indx) {
+
+            if ($(this).attr("data-background")) {
+                $(this).css("background-image", "url(" + $(this).data("background") + ")");
+            }
+        });
+        
+        /*------------------------------------
+            05. Video
+        --------------------------------------*/
+
+        // It is for local video
+        $('.story-video').magnificPopup({
+            delegate: '.video',
+            type: 'iframe'
+        });
+
+        $('.popup-social-video').magnificPopup({
+                disableOn: 700,
+                type: 'iframe',
+                mainClass: 'mfp-fade',
+                removalDelay: 160,
+                preloader: false,
+                fixedContentPos: false
+        });
+
+        /*------------------------------------
+            06. Resize function
+        --------------------------------------*/
+
+        $window.resize(function(event) {
+            setTimeout(function() {
+                SetResizeContent();
+            }, 500);
+            event.preventDefault();
+        });
+
+        /*------------------------------------
+            07. FullScreenHeight function
+        --------------------------------------*/
+
+        function fullScreenHeight() {
+            var element = $(".full-screen");
+            var $minheight = $window.height();
+            element.css('min-height', $minheight);
+        }
+
+        /*------------------------------------
+            08. ScreenFixedHeight function
+        --------------------------------------*/
+
+        function ScreenFixedHeight() {
+            var $headerHeight = $("header").height();
+            var element = $(".screen-height");
+            var $screenheight = $window.height() - $headerHeight;
+            element.css('height', $screenheight);
+        }
+
+        /*------------------------------------
+            09. FullScreenHeight and screenHeight with resize function
+        --------------------------------------*/        
+
+        function SetResizeContent() {
+            fullScreenHeight();
+            ScreenFixedHeight();
+        }
+
+        SetResizeContent();
+
+    // === when document ready === //
+    $(document).ready(function(){
+
+
+        /*------------------------------------
+            10. Sliders
+        --------------------------------------*/
+
+        // popular-courses-carousel
+        $('.popular-courses-carousel').owlCarousel({
+            loop: true,
+            responsiveClass: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            smartSpeed: 1500,            
+            nav: false,
+            dots: true,
+            margin: 30,
+            responsive: {
+                0: {
+                    items: 1,
+                    margin: 20
+                },
+                768: {
+                    items: 2
+                },
+                1200: {
+                    items: 3
+                }
+            }
+        });
+
+        // misson-carousel
+        $('.misson-carousel').owlCarousel({
+            loop: true,
+            responsiveClass: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            smartSpeed: 1500,            
+            nav: false,
+            dots: false,
+            margin: 0,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                576: {
+                    items: 2
+                },
+                992: {
+                    items: 3
+                },
+                1400: {
+                    items: 4
+                }
+            }
+        });
+
+        // testmonial-carousel
+        $('.testimonial-carousel').owlCarousel({
+            loop: true,
+            responsiveClass: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            smartSpeed: 1500,
+            nav: false,
+            dots: true,
+            center:false,
+            margin: 30,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 2
+                }
+            }
+        });
+
+        // client-carousel
+        $('.client-carousel').owlCarousel({
+            loop: true,
+            responsiveClass: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            smartSpeed: 1500,
+            nav: false,
+            dots: false,
+            center:false,
+            margin: 0,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                481: {
+                    items: 2
+                },
+                768: {
+                    items: 3
+                },
+                992: {
+                    items: 4
+                }
+            }
+        });
+
+        // blog-carousel
+        $('.blog-carousel').owlCarousel({
+            loop: true,
+            responsiveClass: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            smartSpeed: 1500,
+            nav: false,
+            dots: true,
+            center:false,
+            margin: 30,
+            responsive: {
+                0: {
+                    items: 1,
+                    margin: 0
+                },
+                768: {
+                    items: 1
+                },
+                992: {
+                    items: 2
+                },
+                1200:{
+                    items: 2
+                }
+
+            }
+        });
+
+        // single-project-carousel
+        $('.portfolio-details-carousel').owlCarousel({
+            loop: true,
+            responsiveClass: true,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            smartSpeed: 1500,            
+            nav: false,
+            center: false,
+            dots: true,
+            margin: 20,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 1
+                },
+                992: {
+                    items: 1
+                }
+            }
+        });
+
+        // project-details carousel
+        $('.related-portfolio-carousel').owlCarousel({
+            loop: true,
+            responsiveClass: true,
+            autoplay: true,
+            smartSpeed: 900,            
+            nav: false,
+            dots: true,
+            center: false,
+            margin: 30,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                767: {
+                    items: 1
+                },
+                768: {
+                    items: 2
+                },
+                992: {
+                    items: 3
+                }
+            }
+        });
+        
+        // Default owlCarousel
+        $('.owl-carousel').owlCarousel({
+            items: 1,
+            loop:true,
+            dots: false,
+            margin: 0,
+            autoplay:true,
+            smartSpeed:500
+        });   
+
+        /*------------------------------------
+            11. Tabs
+        --------------------------------------*/
+
+        //Horizontal Tab
+        if ($(".horizontaltab").length !== 0) {
+            $('.horizontaltab').easyResponsiveTabs({
+                type: 'default', //Types: default, vertical, accordion
+                width: 'auto', //auto or any width like 600px
+                fit: true, // 100% fit in a container
+                tabidentify: 'hor_1', // The tab groups identifier
+                activate: function(event) { // Callback function if tab is switched
+                    var $tab = $(this);
+                    var $info = $('#nested-tabInfo');
+                    var $name = $('span', $info);
+                    $name.text($tab.text());
+                    $info.show();
+                }
+            });
+        }
+
+        /*------------------------------------
+            12. CountUp
+        --------------------------------------*/
+
+        $('.countup').counterUp({
+            delay: 25,
+            time: 2000
+        });
+
+        /*------------------------------------
+            13. Countdown
+        --------------------------------------*/
+
+        // CountDown for coming soon page
+        $(".countdown").countdown({
+                date: "01 Jan 2024 00:01:00", //set your date and time. EX: 15 May 2014 12:00:00
+                format: "on"
+        });
+      
+    });
+
+    // === when window loading === //
+    $window.on("load", function() {
+
+        /*------------------------------------
+            14. Isotop
+        --------------------------------------*/
+
+        var $PortfolioGallery = $('.portfolio-gallery').isotope({
+            // options
+        });
+
+        // filter items on button click
+        $('.filtering').on('click', 'span', function() {
+            var filterValue = $(this).attr('data-filter');
+            $PortfolioGallery.isotope({
+                filter: filterValue
+            });
+        });
+
+        $('.filtering').on('click', 'span', function() {
+            $(this).addClass('active').siblings().removeClass('active');
+        });
+
+        $('.portfolio-gallery').lightGallery();
+
+        // stellar
+        $window.stellar();
+
+    });
+
+})(jQuery);
